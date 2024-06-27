35d9b152f0462cd87a9315b565239524a689c9e2
srila <srilakshmiannamdasu@gmail.com>
2024-06-27 14:18:04 +0530

    Student

---
+++ src/project/Student.java
@@ -0,0 +1,14 @@
+package project;
+
+public class Student {
+    String studentName;
+    String studentId;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentName='" + studentName + '\'' +
+                ", studentId='" + studentId + '\'' +
+                '}';
+    }
+}
---
+++ src/project/StudentTest.java
@@ -0,0 +1,10 @@
+package project;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.studentName = "Sri";
+        student.studentId = "ABS";
+        System.out.println(student);
+    }
+}
